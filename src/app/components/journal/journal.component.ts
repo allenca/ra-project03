b97ba4405f20b5d1d1e711690f032d48877aa7bb
allenca <allen.ca.chan@gmail.com>
2017-03-02 19:24:02 -0500

    006
    Producing the objects from the array of data by:
    -Removing references to owl carousel to minimize chance of errors
    -Building a simple js for loop to bring the objects out of the array of
    posts

--- src/app/components/journal/journal.component.ts
+++ src/app/components/journal/journal.component.ts
@@ -30,43 +30,56 @@ export class JournalComponent implements OnInit {
  getJournals(): void {
     console.log('getting journals');
     let myPromiseOfJournals:Promise<JournalEntry[]> = this.JournalService.getJournals();
-
+    //console.log(myPromiseOfJournals);
    myPromiseOfJournals.then(
         journals => {
 
        this.Journals = <JournalEntry[]>journals;
             
-            // console.log(this.Journals)
+       console.log(this.Journals)
         
         // console.log(this.journalEntries);
         // console.log("***** in journal.component.ts callback *****");
         
-        }).then(run =>{this.owlCarousel();});
+        }).then(
+        run =>{
+            for(let i=0;i<this.Journals.length;i++){
+             let journal:JournalEntry = this.Journals[i];
+           
+             let journalTitle:string = journal.title;
+             let journalContent:string = journal.content;
+             let journalImage:string = journal.image;
+
+             //document.write(journalTitle);
+           
+
+            }
+        });
   }
-  owlCarousel() {
-    $(document).ready(function(){
-            $('.owl-carousel').owlCarousel({
-              loop:true,
-              margin:10,
-              responsiveClass:true,
-              responsive:{
-                  0:{
-                      items:1,
-                      nav:true
-                  },
-                  600:{
-                      items:2,
-                      nav:false
-                  },
-                  1050:{
-                      items:3,
-                      nav:true,
-                      loop:false
-                    }
-                }
-            })
+//   owlCarousel() {
+//     $(document).ready(function(){
+//             $('.owl-carousel').owlCarousel({
+//               loop:true,
+//               margin:10,
+//               responsiveClass:true,
+//               responsive:{
+//                   0:{
+//                       items:1,
+//                       nav:true
+//                   },
+//                   600:{
+//                       items:2,
+//                       nav:false
+//                   },
+//                   1050:{
+//                       items:3,
+//                       nav:true,
+//                       loop:false
+//                     }
+//                 }
+//             })
 
-       });
+//        });
 
-}
+// }
 }
